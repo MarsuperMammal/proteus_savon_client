152b7773f5e99566ee0bd1fa715873f2b69507a1
Phil Watts <pwatts217@gmail.com>
2015-05-25 11:18:02 -0400

    Update bluecat_prov_up.rb

--- quick_fix/bluecat_prov_up.rb
+++ quick_fix/bluecat_prov_up.rb
@@ -1,6 +1,6 @@
 require './bluecat_prov.rb'
 ipaddress=Facter.value(:ipaddress)
 fqdn=Facter.value(:fqdn)
-prov=Bluecat::Api.new('https://ipam.tycoelectronics.net:/Services/API?wsdl','te0s0067','/Thgq0*0wa')
+prov=Bluecat::Api.new()
 prov.set_sys_host_record('32746', fqdn, ipaddress)
 p "A Record #{fqdn} created for #{ipaddress}"
