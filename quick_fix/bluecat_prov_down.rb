0d4b9695b12b1b7228b8abed1e12b799e006b69b
Phil Watts <pwatts217@gmail.com>
2015-05-25 11:17:46 -0400

    Update bluecat_prov_down.rb

--- quick_fix/bluecat_prov_down.rb
+++ quick_fix/bluecat_prov_down.rb
@@ -1,5 +1,5 @@
 require './bluecat_prov.rb'
 fqdn=Facter.value(:fqdn)
-prov=Bluecat::Api.new('https://ipam.tycoelectronics.net:/Services/API?wsdl','te0s0067','/Thgq0*0wa')
+prov=Bluecat::Api.new()
 prov.remove_sys_dns_identity(fqdn)
 p "DNS Enteries removed"
