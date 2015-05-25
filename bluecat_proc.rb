78b78dcfb103d80d7672f5adabfe4fb498a737ea
Phil Watts <pwatts217@gmail.com>
2015-05-25 11:17:00 -0400

    Update bluecat_proc.rb

--- bluecat_proc.rb
+++ bluecat_proc.rb
@@ -1,4 +1,4 @@
 require './bluecat_api.rb'
-  poc = Bluecat::Api.new('https://ipam.tycoelectronics.net:/Services/API?wsdl','te0s0067','/Thgq0*0wa')
+  poc = Bluecat::Api.new('https://ipam.tycoelectronics.net:/Services/API?wsdl','','')
 
 
